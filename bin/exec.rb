a538529361a4321d21d672070ca9854d8b565a5e
Shoma FUKUDA <fkshom@gmail.com>
2022-04-19 00:38:28 +0900

    wi

--- bin/exec.rb
+++ bin/exec.rb
@@ -1,11 +1,11 @@
 $LOAD_PATH[0, 0] = File.join(File.dirname(__FILE__), '..', 'lib')
-
+require 'erb'
 require 'dotenv'
 require 'gitlab'
 Dotenv.load
 
 Gitlab.configure do |config|
-  config.endpoint       = 'https://gitlab.com/api/v4'
+  config.endpoint       = ENV['GITLAB_ENDPOINT']
   config.private_token  = ENV['GITLAB_TOKEN']
 end
 
@@ -60,31 +60,6 @@ end
 
 gitlabissues = GitlabIssues.new
 
-# priority_low = gitlabissues.group_issues(group, {
-#                                            labels: 'Priority: Low'
-#                                          })
-# puts 'priority_low'
-# puts '=' * 50
-# pp priority_low
-
-# discussions_by_user = gitlabissues.discussions(discussion_group_project, discussion_issue_id, {})
-# puts 'discussions_by_user'
-# puts '=' * 50
-# pp discussions_by_user
-
-# discussion_issue = gitlabissues.issue(discussion_group_project, discussion_issue_id)
-# puts 'discussion_issue'
-# puts '=' * 50
-# pp discussion_issue
-
-# priority_low_and_type_feature = gitlabissues.group_issues(group, {
-#                                                             labels: 'Priority: Low,Type: Feature'
-#                                                           })
-# puts 'priority_low_and_type_feature'
-# puts '=' * 50
-# pp priority_low_and_type_feature
-
-
 class GitlabWrapper
   def initialize
     clear_cache
@@ -105,14 +80,14 @@ class GitlabWrapper
     self
   end
 
-  def issue(params, options)
+  def issue(params, _options)
     Gitlab.issue(*params)
   end
 
   def group_issues(params, options)
     result = []
     Gitlab.group_issues(*params, options).each_page do |items|
-      result += items.map{|item| item.to_hash}
+      result += items.map { |item| item.to_hash }
     end
     result
   end
@@ -120,7 +95,7 @@ class GitlabWrapper
   def issue_notes(params, options)
     result = []
     Gitlab.issue_notes(*params, options).each_page do |items|
-      result += items.map{|item| item.to_hash}
+      result += items.map { |item| item.to_hash }
     end
     result.reject! do |note|
       note['system'] == true
@@ -128,50 +103,15 @@ class GitlabWrapper
     result
   end
 
-  def call(options={})
+  def call(options = {})
     new_options = @options.update(options)
     new_options = new_options.update(per_page: 100)
     query_id = @query.to_s + @params.hash.to_s + new_options.hash.to_s
-    unless @cache.key?(query_id)
-      @cache[query_id] = self.send(@query, @params, new_options)
-    end
+    @cache[query_id] = send(@query, @params, new_options) unless @cache.key?(query_id)
     @cache[query_id]
   end
 end
 
-group_issues = GitlabWrapper.new.query(:group_issues).params(group, milestone: nil)
-discussions = GitlabWrapper.new.query(:issue_notes).params(
-  discussion_group_project, discussion_issue_id
-)
-discussion_issue = GitlabWrapper.new.query(:issue).params(
-  discussion_group_project, discussion_issue_id
-)
-puts <<~"EOS"
-  - issue数
-    - Priority Low: #{group_issues.(labels: 'Priority: Low,Type: Feature').count}
-      - うち、Type: Feature: #{group_issues.(labels: 'Priority: Low,Type: Feature').count}
-  - コメント数: #{discussions.().count} ( #{discussion_issue.()['title']} )
-EOS
-
-issues_group_by_epic = {}
-gitlabissues.group_issues(group, {
-                            labels: 'Priority: Low'
-                          }).each do |issue|
-  epicname = if issue['epic']
-               issue['epic']['title']
-             else
-               'その他'
-             end
-  issues_group_by_epic[epicname] ||= []
-  issues_group_by_epic[epicname] << issue
-end
-
-other_issues = issues_group_by_epic.delete('その他')
-issues_group_by_epic['その他'] = other_issues if other_issues
-
-issues_group_by_epic.each do |epicname, issues|
-  puts "- #{epicname}"
-  issues.each do |issue|
-    puts "  - #{issue['title']}"
-  end
-end
+template = File.read('template.erb')
+milestone = ARGV[0]
+puts ERB.new(template).run
